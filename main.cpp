47be9dac38ea100cdc01645e9f242a1c20f82fe9
DiegoSandoval97 <dsandoval@fi.uba.ar>
2023-03-27 18:37:50 +0000

    C code

--- main.cpp
+++ main.cpp
@@ -3,21 +3,36 @@
 
 int main()
 {
-    DigitalIn gasDetector(D3);
+    /**
+    * @brief Declarar las variables correspondientes a un pin digital de entrada (gasDetector)
+    *       y a un pin digital de salida (alarmLed).
+    * @param D2 
+    **/
+    //DigitalIn gasDetector(D3);
+    gpio_t gasDetector;
+    gpio_init_in(&gasDetector, D2);
 
-    DigitalOut alarmLed(LED1);
+    //DigitalOut alarmLed(LED1);
+    //gasDetector.mode(PullDown);
+    gpio_t alarmLed;
+    gpio_init_out_ex(&alarmLed, LED1, PullDown);
 
-    gasDetector.mode(PullDown);
+    //alarmLed = OFF;
+    gpio_write(&alarmLed, OFF);
 
-    alarmLed = OFF;
-    printf("%s\n", "Hello World xD Estoy en el Branch B");
+    printf("%s\n", "Hello World");
+
     while (true) {
-        if ( gasDetector == ON ) {
-            alarmLed = ON;
+        //if ( gasDetector == ON )
+        if ( gpio_read(&gasDetector) == ON ) {
+            //alarmLed = ON;
+            gpio_write(&alarmLed, ON);
         }
         
-        if ( gasDetector == OFF ) {
-            alarmLed = OFF;
+        //if ( gasDetector == OFF )
+        if ( gpio_read(&gasDetector) == OFF ) {
+            //alarmLed = OFF;
+            gpio_write(&alarmLed, OFF);
         }
     }
 }
